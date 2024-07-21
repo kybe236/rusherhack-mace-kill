e3169cc03a4d53d04abd27caf4fd29d41d79283e
Kyrill Belomenov <apfel236@outlook.de>
2024-07-21 11:33:48 +0200

    improvements

--- src/main/java/org/kybe/MazeKiller.java
+++ src/main/java/org/kybe/MazeKiller.java
@@ -2,10 +2,9 @@ package org.kybe;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 import net.minecraft.network.protocol.game.ServerboundMoveVehiclePacket;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
@@ -15,6 +14,7 @@ import org.rusherhack.client.api.feature.module.ToggleableModule;
 import org.rusherhack.client.api.utils.ChatUtils;
 import org.rusherhack.core.event.subscribe.Subscribe;
 import org.rusherhack.core.setting.BooleanSetting;
+import org.rusherhack.core.setting.NumberSetting;
 
 
 /**
@@ -27,6 +27,7 @@ public class MazeKiller extends ToggleableModule {
 	/**
 	 * Settings
 	 */
+	private final NumberSetting<Integer> height = new NumberSetting<>("Height", "Height", 170, 1, 170);
 	private final BooleanSetting maximum = new BooleanSetting("Maximum", "Maximum", true);
 
 
@@ -38,13 +39,12 @@ public class MazeKiller extends ToggleableModule {
 
 		//register settings
 		this.registerSettings(
+				this.height,
 				this.maximum
 		);
 	}
 
-	private Vec3 previouspos;
-
-	@Subscribe
+    @Subscribe
 	public void onUpdate(EventInteract event) {
 		//check if the module is enabled
 		if (!this.isToggled()) {
@@ -52,24 +52,18 @@ public class MazeKiller extends ToggleableModule {
 		}
 
 		try {
-			Class<?> packetClass = event.getClass();
-
 			if (!event.getAction().equals(EventInteract.Action.ATTACK)) {
-				ChatUtils.print("Not attacking");
 				return;
 			}
-			if (!(event.getTargetEntity() instanceof LivingEntity targetEntity)) {
-				ChatUtils.print("Not living entity");
-				return;
-			}
-			if (!(mc.player.getMainHandItem().getItem() == Items.MACE)) {
-				ChatUtils.print("Not holding mace");
+            assert mc.player != null;
+            if (!(mc.player.getMainHandItem().getItem() == Items.MACE)) {
 				return;
 			}
 
-            if (event.isCancelled() && ((targetEntity.isBlocking() && targetEntity.isBlocking()) || targetEntity.isInvulnerable())) return;
+			Entity targetEntity = event.getTargetEntity();
+            if (event.isCancelled() || targetEntity.isInvulnerable()) return;
 
-			previouspos = new Vec3(targetEntity.getX(), targetEntity.getY(), targetEntity.getZ());
+            Vec3 previouspos = new Vec3(targetEntity.getX(), targetEntity.getY(), targetEntity.getZ());
 
 			int blocks = getMaxHeightAbovePlayer();
 
@@ -78,42 +72,38 @@ public class MazeKiller extends ToggleableModule {
 				return;
 			}
 
-			int packetsRequired = (int) Math.ceil(Math.abs(blocks / 10));
+			int packetsRequired = (int) (double) Math.abs(blocks / 10);
 
 			if (packetsRequired > 20) {
 				packetsRequired = 1;
 			}
 
-			if (true) {
-				if (mc.player.getVehicle() != null) {
-					for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-						mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
-					}
-					mc.player.getVehicle().setPos(mc.player.getVehicle().getX(), mc.player.getVehicle().getY() + blocks, mc.player.getVehicle().getZ());
-					mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
-				} else {
-					for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-						mc.player.connection.send(new ServerboundMovePlayerPacket.StatusOnly(false));
-					}
-					mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(mc.player.getX(), mc.player.getY() + blocks, mc.player.getZ(), false));
-				}
+            if (mc.player.getVehicle() != null) {
+                for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
+                    mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
+                }
+                mc.player.getVehicle().setPos(mc.player.getVehicle().getX(), mc.player.getVehicle().getY() + blocks, mc.player.getVehicle().getZ());
+                mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
+            } else {
+                for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
+                    mc.player.connection.send(new ServerboundMovePlayerPacket.StatusOnly(false));
+                }
+                mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(mc.player.getX(), mc.player.getY() + blocks, mc.player.getZ(), false));
+            }
 
-				if (mc.player.getVehicle() != null) {
-					mc.player.getVehicle().setPos(previouspos);
-					mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
-					// Do it again to be sure it happens
-					mc.player.getVehicle().setPos(previouspos);
-					mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
-				} else {
-					mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(previouspos.get(Direction.Axis.X), previouspos.get(Direction.Axis.Y), previouspos.get(Direction.Axis.Z), false));
-					// Do it again to be sure it happens
-					mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(previouspos.get(Direction.Axis.X), previouspos.get(Direction.Axis.Y), previouspos.get(Direction.Axis.Z), false));
-				}
-			}else {
-				ChatUtils.print("Not safe block");
-			}
+            if (mc.player.getVehicle() != null) {
+                mc.player.getVehicle().setPos(previouspos);
+                mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
+                // Do it again to be sure it happens
+                mc.player.getVehicle().setPos(previouspos);
+                mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
+            } else {
+                mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(previouspos.get(Direction.Axis.X), previouspos.get(Direction.Axis.Y), previouspos.get(Direction.Axis.Z), false));
+                // Do it again to be sure it happens
+                mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(previouspos.get(Direction.Axis.X), previouspos.get(Direction.Axis.Y), previouspos.get(Direction.Axis.Z), false));
+            }
 
-		} catch (Exception e) {
+        } catch (Exception e) {
 			ChatUtils.print("Error: " + e.getMessage());
 		}
 
@@ -122,7 +112,8 @@ public class MazeKiller extends ToggleableModule {
 	private int getMaxHeightAbovePlayer() {
         assert mc.player != null;
         BlockPos playerPos = new BlockPos(mc.player.getBlockX(), mc.player.getBlockY(), mc.player.getBlockZ());
-		int maxHeight = playerPos.getY() + 170;
+		int maxHeight = maximum.getValue() ? 170 : height.getValue();
+		maxHeight += playerPos.getY();
 
 		for (int i = maxHeight; i > playerPos.getY(); i--) {
 			BlockPos isopenair1 = new BlockPos(playerPos.getX(), i, playerPos.getZ());
@@ -136,7 +127,7 @@ public class MazeKiller extends ToggleableModule {
 
 	private boolean isSafeBlock(BlockPos pos) {
         assert mc.level != null;
-		return mc.level.getBlockState(pos).getBlock().equals(Blocks.AIR);
+		return mc.level.getBlockState(pos).getBlock().equals(Blocks.AIR) || mc.level.getBlockState(pos).getBlock().equals(Blocks.WATER) || mc.level.getBlockState(pos).getBlock().equals(Blocks.POWDER_SNOW);
 	}
 
 }

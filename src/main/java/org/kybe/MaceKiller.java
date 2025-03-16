ab7b95b24ba98f7edd91aa1fbc878f6fa11e0b1c
Kyrill Belomenov <apfel236@outlook.de>
2025-03-16 19:21:51 +0100

    maybe works

--- src/main/java/org/kybe/MaceKiller.java
+++ src/main/java/org/kybe/MaceKiller.java
@@ -24,111 +24,117 @@ import org.rusherhack.core.setting.NumberSetting;
  */
 public class MaceKiller extends ToggleableModule {
 
-        /**
-         * Settings
-         */
-        private final NumberSetting<Integer> height = new NumberSetting<>("Height", "Height", 170, 1, 170);
-        private final BooleanSetting maximum = new BooleanSetting("Maximum", "Maximum", true);
+	/**
+	 * Settings
+	 */
+	private final NumberSetting<Integer> height = new NumberSetting<>("Height", "Height", 170, 1, 170);
+	private final BooleanSetting maximum = new BooleanSetting("Maximum", "Maximum", true);
 
 
-        /**
-         * Constructor
-         */
-        public MaceKiller() {
-                super("Mace Killer", "Makes more damage with mace", ModuleCategory.CLIENT);
+	/**
+	 * Constructor
+	 */
+	public MaceKiller() {
+		super("Mace Killer", "Makes more damage with mace", ModuleCategory.CLIENT);
 
-                //register settings
-                this.registerSettings(
-                                this.height,
-                                this.maximum
-                );
-        }
+		//register settings
+		this.registerSettings(
+				this.height,
+				this.maximum
+		);
+	}
 
-        @SuppressWarnings("ConstantConditions")
-        @Subscribe
+	@SuppressWarnings("ConstantConditions")
+	@Subscribe
+	public void onUpdate(EventInteract event) {
+		//check if the module is enabled
+		if (!this.isToggled()) {
+			return;
+		}
 
-        public void onUpdate(EventInteract event) {
-                //check if the module is enabled
-                if (!this.isToggled()) {
-                        return;
-                }
+		try {
+			if (!event.getAction().equals(EventInteract.Action.ATTACK)) {
+				return;
+			}
+			if (!(mc.player.getMainHandItem().getItem() == Items.MACE)) {
+				return;
+			}
 
-                try {
-                        if (!event.getAction().equals(EventInteract.Action.ATTACK)) {
-                                return;
-                        }
-            if (!(mc.player.getMainHandItem().getItem() == Items.MACE)) {
-                                return;
-                        }
+			Entity targetEntity = event.getTargetEntity();
+			if (event.isCancelled() || targetEntity.isInvulnerable()) return;
 
-                        Entity targetEntity = event.getTargetEntity();
-            if (event.isCancelled() || targetEntity.isInvulnerable()) return;
+			Vec3 previouspos = new Vec3(mc.player.getX(), mc.player.getY(), mc.player.getZ());
+			int blocks = getMaxHeightAbovePlayer();
 
-            Vec3 previouspos = new Vec3(mc.player.getX(), mc.player.getY(), mc.player.getZ());
+			if (blocks == 0) {
+				ChatUtils.print("No suitable position found");
+				return;
+			}
 
-                        int blocks = getMaxHeightAbovePlayer();
+			int packetsRequired = (int) (double) Math.abs(blocks / 10);
 
-                        if (blocks == 0) {
-                                ChatUtils.print("No suitable position found");
-                                return;
-                        }
+			if (packetsRequired > 20) {
+				packetsRequired = 1;
+			}
 
-                        int packetsRequired = (int) (double) Math.abs(blocks / 10);
+			if (mc.player.getVehicle() != null) {
+				for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
+					mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
+				}
+				mc.player.getVehicle().setPos(mc.player.getVehicle().getX(), mc.player.getVehicle().getY() + blocks, mc.player.getVehicle().getZ());
+				mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
+			} else {
+				for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
+					mc.player.connection.send(new ServerboundMovePlayerPacket.StatusOnly(false));
+				}
+				mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(mc.player.getX(), mc.player.getY() + blocks, mc.player.getZ(), false));
+			}
 
-                        if (packetsRequired > 20) {
-                                packetsRequired = 1;
-                        }
+			if (mc.player.getVehicle() != null) {
+				mc.player.getVehicle().setPos(previouspos);
+				mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
+				// Do it again to be sure it happens
+				mc.player.getVehicle().setPos(previouspos);
+				mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
+			} else {
+				mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(previouspos.get(Direction.Axis.X), previouspos.get(Direction.Axis.Y), previouspos.get(Direction.Axis.Z), false));
+				// Do it again to be sure it happens
+				mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(previouspos.get(Direction.Axis.X), previouspos.get(Direction.Axis.Y), previouspos.get(Direction.Axis.Z), false));
+				// and again because it failed sometimes
+				mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(previouspos.get(Direction.Axis.X), previouspos.get(Direction.Axis.Y), previouspos.get(Direction.Axis.Z), false));
+			}
 
-            if (mc.player.getVehicle() != null) {
-                for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-                    mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
-                }
-                mc.player.getVehicle().setPos(mc.player.getVehicle().getX(), mc.player.getVehicle().getY() + blocks, mc.player.getVehicle().getZ());
-                mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
-            } else {
-                for (int packetNumber = 0; packetNumber < (packetsRequired - 1); packetNumber++) {
-                    mc.player.connection.send(new ServerboundMovePlayerPacket.StatusOnly(false));
-                }
-                mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(mc.player.getX(), mc.player.getY() + blocks, mc.player.getZ(), false));
-            }
+		} catch (Exception e) {
+			ChatUtils.print("Error: " + e.getMessage());
+		}
 
-            if (mc.player.getVehicle() != null) {
-                mc.player.getVehicle().setPos(previouspos);
-                mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
-                // Do it again to be sure it happens
-                mc.player.getVehicle().setPos(previouspos);
-                mc.player.connection.send(new ServerboundMoveVehiclePacket(mc.player.getVehicle()));
-            } else {
-                mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(previouspos.get(Direction.Axis.X), previouspos.get(Direction.Axis.Y), previouspos.get(Direction.Axis.Z), false));
-                // Do it again to be sure it happens
-                mc.player.connection.send(new ServerboundMovePlayerPacket.Pos(previouspos.get(Direction.Axis.X), previouspos.get(Direction.Axis.Y), previouspos.get(Direction.Axis.Z), false));
-            }
+	}
 
-        } catch (Exception e) {
-                        ChatUtils.print("Error: " + e.getMessage());
-                }
+	private int getMaxHeightAbovePlayer() {
+		if (mc.player == null) {
+			return 0;
+		}
 
-        }
+		BlockPos playerPos = new BlockPos(mc.player.getBlockX(), mc.player.getBlockY(), mc.player.getBlockZ());
+		int maxHeight = maximum.getValue() ? 170 : height.getValue();
+		maxHeight += playerPos.getY();
 
-        private int getMaxHeightAbovePlayer() {
-        assert mc.player != null;
-        BlockPos playerPos = new BlockPos(mc.player.getBlockX(), mc.player.getBlockY(), mc.player.getBlockZ());
-                int maxHeight = maximum.getValue() ? 170 : height.getValue();
-                maxHeight += playerPos.getY();
+		for (int i = maxHeight; i > playerPos.getY(); i--) {
+			BlockPos isopenair1 = new BlockPos(playerPos.getX(), i, playerPos.getZ());
+			BlockPos isopenair2 = isopenair1.above(1);
+			if (isSafeBlock(isopenair1) && isSafeBlock(isopenair2)) {
+				return i - playerPos.getY();
+			}
+		}
 
-                for (int i = maxHeight; i > playerPos.getY(); i--) {
-                        BlockPos isopenair1 = new BlockPos(playerPos.getX(), i, playerPos.getZ());
-                        BlockPos isopenair2 = isopenair1.above(1);
-                        if (isSafeBlock(isopenair1) && isSafeBlock(isopenair2)) {
-                                return i - playerPos.getY();
-                        }
-                }
-                return 0; // Return 0 if no suitable position is found
-        }
+		return 0; // Return 0 if no suitable position is found
+	}
 
-        private boolean isSafeBlock(BlockPos pos) {
-        assert mc.level != null;
-                return mc.level.getBlockState(pos).getBlock().equals(Blocks.AIR) || mc.level.getBlockState(pos).getBlock().equals(Blocks.WATER) || mc.level.getBlockState(pos).getBlock().equals(Blocks.POWDER_SNOW);
-        }
+	private boolean isSafeBlock(BlockPos pos) {
+		if (mc.level == null) {
+			return false;
+		}
+		return mc.level.getBlockState(pos).getBlock().equals(Blocks.AIR) || mc.level.getBlockState(pos).getBlock().equals(Blocks.WATER) || mc.level.getBlockState(pos).getBlock().equals(Blocks.POWDER_SNOW);
+	}
 
 }
--- src/main/java/org/kybe/Main.java
+++ src/main/java/org/kybe/Main.java
@@ -4,26 +4,26 @@ import org.rusherhack.client.api.RusherHackAPI;
 import org.rusherhack.client.api.plugin.Plugin;
 
 /**
- * Maze insta kill 
+ * Maze insta kill
  *
  * @author kybe236
  */
 public class Main extends Plugin {
-	
+
 	@Override
 	public void onLoad() {
-		
+
 		//logger
 		this.getLogger().info("[KYBES-MACE-KILLER] loaded");
-		
+
 		//creating and registering a new module
 		final MaceKiller macekiller = new MaceKiller();
 		RusherHackAPI.getModuleManager().registerFeature(macekiller);
 	}
-	
+
 	@Override
 	public void onUnload() {
 		this.getLogger().info("[KYBES-MACE-KILLER] unloaded");
 	}
-	
+
 }
